bed3ec2a55937330d0f69f2eb9abc4ebcd9378f8
彭定林 <1979945716@qq.com>
2019-02-19 01:26:03 +0800

    header组件添加picker

--- app/components/Header/ToggleSpan.js
+++ app/components/Header/ToggleSpan.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 
 const ToggleSpan = styled.span`
-    color: #fff;
-    font-size: 0.1rem;
+  color: #fff;
+  font-size: 0.8rem;
 `
 
 export default ToggleSpan
--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { Picker } from 'antd-mobile'
 import Wrapper from './Wrapper'
 import Toggle from './Toggle'
+
+const data = [
+  {
+    label: '北京',
+    value: '北京',
+  },
+  {
+    label: '天津',
+    value: '天津',
+  },
+]
+
+const CustomChildren = props => (
+  <div onClick={props.onClick} role="presentation">
+    <Toggle extra={props.extra} />
+  </div>
+)
+
+CustomChildren.propTypes = {
+  onClick: PropTypes.func,
+  extra: PropTypes.string,
+}
 
 function Header() {
   return (
     <Wrapper>
-      <Toggle />
+      <Picker
+        data={data}
+        cols={1}
+        onOk={value => {
+          console.log(value)
+        }}
+      >
+        <CustomChildren />
+      </Picker>
     </Wrapper>
   )
 }
